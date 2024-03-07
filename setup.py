615d564a753f6f1e97b9f3169914353e8e9339a3
Alex <56325539+asigalov61@users.noreply.github.com>
2024-03-07 10:42:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='chords_recognizer',
@@ -8,6 +8,9 @@ setup(
     author='Yikai Liao',
     author_email='your.email@example.com',
     license='MIT',
-    packages=['mido'],
+    packages=find_packages(),  # Automatically find all packages
+    install_requires=[
+        'mido',  # Add any other dependencies here
+    ],
     zip_safe=False
 )
