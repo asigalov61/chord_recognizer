6585099312a07b8d3253424ddfe276c4ec56e703
Alex <56325539+asigalov61@users.noreply.github.com>
2024-03-07 10:36:15 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='chords_recognizer',
+    version='0.0.1',
+    description='Working and installable chords_recognizer repo mirror copy',
+    url='https://github.com/Yikai-Liao/chord_recognizer',
+    author='Yikai Liao',
+    author_email='your.email@example.com',
+    license='MIT',
+    packages=['mido'],
+    zip_safe=False
+)
