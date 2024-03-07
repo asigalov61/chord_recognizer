6bad5da03dbd0dd8607ec5c36e3fc7c91f23d9f2
Alex <56325539+asigalov61@users.noreply.github.com>
2024-03-07 11:27:15 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     author_email='your.email@example.com',
     license='MIT',
     packages=find_packages(),
-    package_data={
-        '': ['test_data/*/*'],  # Include all files under test_data directory
-    },
     include_package_data=True,  # This line includes data files specified in MANIFEST.in
+    package_data={
+    '': ['test_data/*'],  # Include all files under test_data directory
+    },
     install_requires=[
         'mido',  # Add any other dependencies here
     ],
