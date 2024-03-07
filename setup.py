6473cc449804eec3fe5fe3ab81ae9e8bf83566c2
Alex <56325539+asigalov61@users.noreply.github.com>
2024-03-07 11:32:34 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,  # This line includes data files specified in MANIFEST.in
     package_data={
-    '': ['test_data/*'],  # Include all files under test_data directory
+    '': ['test_data'],  # Include all files under test_data directory
     },
     install_requires=[
         'mido',  # Add any other dependencies here
